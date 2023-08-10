a4e008d2936cbba7ccc027c743198a12e615a5cb
Abner <abnerisraelrosales@hotmail.com>
2023-08-09 21:52:28 -0600

    In the JavaScript file, define the variables that will be used.

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+alert("In this exercise, you will be asked 5 multiple-choice questions, and upon answering all of them, we will display the type of clothing we would recommend you to wear.")
+
+var gender = prompt("What is your gender, Girl or Boy?");
+var name = prompt("What is your Name?");
+var age = parseInt(prompt("What is your Age?"));
+var weather = prompt("What type of weather do you prefer, Hot or Cold?")
+var color = prompt("What color you preferred? Red, Blue, Green, Pink, White or Black?");
+
+
+
+console.log("Welcome " + name + "! This is the clothes recommendation for you 🤩:");
+
+
+
