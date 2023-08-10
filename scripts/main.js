3480c784e1ecef2824e704f59ea13db6ef5e5a96
Abner <abnerisraelrosales@hotmail.com>
2023-08-10 14:51:16 -0600

    I put the line document.write so the message with your name appear on the screen

--- scripts/main.js
+++ scripts/main.js
@@ -7,9 +7,11 @@ var weather = prompt("What type of weather do you prefer, Hot or Cold?")
 var color = prompt("What color you prefer? Red, Blue or Green?");
 
 
-
+//Se muestra en consola//
 console.log("Welcome " + name + "! This is the clothes recommendation for you 🤩:");
 
+//Se muestra en pantalla//
+document.write("WELCOME " + name + "! ");
 
 //Choosing//
 
@@ -17,108 +19,108 @@ console.log("Welcome " + name + "! This is the clothes recommendation for you �
 if ((gender == "Girl" || gender =="girl") && (weather == "Cold" || weather == "cold") && (age == "Child" || age =="child") && (color == "Red" || color == "red")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img1.jpeg'>")
+    document.write("<img src='images/img1.jpeg' width='200' height='200'>")
 }else if ((gender == "Girl" || gender =="girl") && (weather == "Hot" || weather == "hot") && (age == "Child" || age =="child") && (color == "Red" || color == "red")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img4.jpeg'>")
+    document.write("<img src='images/img4.jpeg' width='200' height='200'>")
 }else if ((gender == "Girl" || gender =="girl") && (weather == "Cold" || weather == "cold") && (age == "Child" || age =="child") && (color == "Blue" || color == "blue")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img2.jpeg'>")
+    document.write("<img src='images/img2.jpeg' width='200' height='200'>")
 }else if ((gender == "Girl" || gender =="girl") && (weather == "Hot" || weather == "hot") && (age == "Child" || age =="child") && (color == "Blue" || color == "blue")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img5.jpeg'>")
+    document.write("<img src='images/img5.jpeg' width='200' height='200'>")
 }else if ((gender == "Girl" || gender =="girl") && (weather == "Cold" || weather == "cold") && (age == "Child" || age =="child") && (color == "Green" || color == "green")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img3.jpeg'>")
+    document.write("<img src='images/img3.jpeg' width='200' height='200'>")
 }else if ((gender == "Girl" || gender =="girl") && (weather == "Hot" || weather == "hot") && (age == "Child" || age =="child") && (color == "Green" || color == "green")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img6.jpeg'>")
+    document.write("<img src='images/img6.jpeg' width='200' height='200'>")
 }
 
 //Girl Teenager//
 if ((gender == "Girl" || gender =="girl") && (weather == "Cold" || weather == "cold") && (age == "Teenager" || age =="teenager") && (color == "Red" || color == "red")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img7.jpeg'>")
+    document.write("<img src='images/img7.jpeg' width='200' height='200'>")
 }else if ((gender == "Girl" || gender =="girl") && (weather == "Hot" || weather == "hot") && (age == "Teenager" || age =="teenager") && (color == "Red" || color == "red")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img8.jpeg'>")
+    document.write("<img src='images/img8.jpeg' width='200' height='200'>")
 }else if ((gender == "Girl" || gender =="girl") && (weather == "Cold" || weather == "cold") && (age == "Teenager" || age =="teenager") && (color == "Blue" || color == "blue")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img9.jpeg'>")
+    document.write("<img src='images/img9.jpeg' width='200' height='200'>")
 }else if ((gender == "Girl" || gender =="girl") && (weather == "Hot" || weather == "hot") && (age == "Teenager" || age =="teenager") && (color == "Blue" || color == "blue")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img10.jpeg'>")
+    document.write("<img src='images/img10.jpeg' width='200' height='200'>")
 }else if ((gender == "Girl" || gender =="girl") && (weather == "Cold" || weather == "cold") && (age == "Teenager" || age =="teenager") && (color == "Green" || color == "green")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img11.jpeg'>")
+    document.write("<img src='images/img11.jpeg' width='200' height='200'>")
 }else if ((gender == "Girl" || gender =="girl") && (weather == "Hot" || weather == "hot") && (age == "Teenager" || age =="teenager") && (color == "Green" || color == "green")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img12.jpeg'>")
+    document.write("<img src='images/img12.jpeg' width='200' height='200'>")
 }
 
 //Girl-Adult
 if ((gender == "Girl" || gender =="girl") && (weather == "Cold" || weather == "cold") && (age == "Adult" || age =="adult") && (color == "Red" || color == "red")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img13.jpeg'>")
+    document.write("<img src='images/img13.jpeg' width='200' height='200'>")
 }else if ((gender == "Girl" || gender =="girl") && (weather == "Hot" || weather == "hot") && (age == "Adult" || age =="adult") && (color == "Red" || color == "red")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img14.jpeg'>")
+    document.write("<img src='images/img14.jpeg' width='200' height='200'>")
 }else if ((gender == "Girl" || gender =="girl") && (weather == "Cold" || weather == "cold") && (age == "Adult" || age =="adult") && (color == "Blue" || color == "blue")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img15.jpeg'>")
+    document.write("<img src='images/img15.jpeg' width='200' height='200'>")
 }else if ((gender == "Girl" || gender =="girl") && (weather == "Hot" || weather == "hot") && (age == "Adult" || age =="adult") && (color == "Blue" || color == "blue")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img16.jpeg'>")
+    document.write("<img src='images/img16.jpeg' width='200' height='200'>")
 }else if ((gender == "Girl" || gender =="girl") && (weather == "Cold" || weather == "cold") && (age == "Adult" || age =="adult") && (color == "Green" || color == "green")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img17.jpeg'>")
+    document.write("<img src='images/img17.jpeg' width='200' height='200'>")
 }else if ((gender == "Girl" || gender =="girl") && (weather == "Hot" || weather == "hot") && (age == "Teenager" || age =="teenager") && (color == "Green" || color == "green")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img18.jpeg'>")
+    document.write("<img src='images/img18.jpeg' width='200' height='200'>")
 }
 
 //Boy - Child
 if ((gender == "Boy" || gender =="boy") && (weather == "Cold" || weather == "cold") && (age == "Child" || age =="child") && (color == "Red" || color == "red")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img19.jpeg'>")
+    document.write("<img src='images/img19.jpeg' width='200' height='200'>")
 }else if ((gender == "Boy" || gender =="boy") && (weather == "Hot" || weather == "hot") && (age == "Child" || age =="child") && (color == "Red" || color == "red")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img20.jpeg'>")
+    document.write("<img src='images/img20.jpeg' width='200' height='200'>")
 }else if ((gender == "Boy" || gender =="boy") && (weather == "Cold" || weather == "cold") && (age == "Child" || age =="child") && (color == "Blue" || color == "blue")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img21.jpeg'>")
+    document.write("<img src='images/img21.jpeg' width='200' height='200'>")
 }else if ((gender == "Boy" || gender =="boy") && (weather == "Hot" || weather == "hot") && (age == "Child" || age =="child") && (color == "Blue" || color == "blue")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img22.jpeg'>")
+    document.write("<img src='images/img22.jpeg' width='200' height='200'>")
 }else if ((gender == "Boy" || gender =="boy") && (weather == "Cold" || weather == "cold") && (age == "Child" || age =="child") && (color == "Green" || color == "green")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img23.jpeg'>")
+    document.write("<img src='images/img23.jpeg' width='200' height='200'>")
 }else if ((gender == "Boy" || gender =="boy") && (weather == "Hot" || weather == "hot") && (age == "Child" || age =="child") && (color == "Green" || color == "green")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img24.jpeg'>")
+    document.write("<img src='images/img24.jpeg' width='200' height='200'>")
 }
 
 //Boy - Teenager
@@ -126,52 +128,52 @@ if ((gender == "Boy" || gender =="boy") && (weather == "Cold" || weather == "col
 if ((gender == "Boy" || gender =="boy") && (weather == "Cold" || weather == "cold") && (age == "Teenager" || age =="teenager") && (color == "Red" || color == "red")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img25.jpeg'>")
+    document.write("<img src='images/img25.jpeg' width='200' height='200'>")
 }else if ((gender == "Boy" || gender =="boy") && (weather == "Hot" || weather == "hot") && (age == "Teenager" || age =="teenager") && (color == "Red" || color == "red")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img26.jpeg'>")
+    document.write("<img src='images/img26.jpeg' width='200' height='200'>")
 }else if ((gender == "Boy" || gender =="boy") && (weather == "Cold" || weather == "cold") && (age == "Teenager" || age =="teenager") && (color == "Blue" || color == "blue")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img27.jpeg'>")
+    document.write("<img src='images/img27.jpeg' width='200' height='200'>")
 }else if ((gender == "Boy" || gender =="boy") && (weather == "Hot" || weather == "hot") && (age == "Teenager" || age =="teenager") && (color == "Blue" || color == "blue")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img28.jpeg'>")
+    document.write("<img src='images/img28.jpeg' width='200' height='200'>")
 }else if ((gender == "Boy" || gender =="boy") && (weather == "Cold" || weather == "cold") && (age == "Teenager" || age =="teenager") && (color == "Green" || color == "green")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img29.jpeg'>")
+    document.write("<img src='images/img29.jpeg' width='200' height='200'>")
 }else if ((gender == "Boy" || gender =="boy") && (weather == "Hot" || weather == "hot") && (age == "Teenager" || age =="teenager") && (color == "Green" || color == "green")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img30.jpeg'>")
+    document.write("<img src='images/img30.jpeg' width='200' height='200'>")
 }
 
 //Boy - Adult//
 if ((gender == "Boy" || gender =="boy") && (weather == "Cold" || weather == "cold") && (age == "Adult" || age =="adult") && (color == "Red" || color == "red")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img31.jpeg'>")
+    document.write("<img src='images/img31.jpeg' width='200' height='200'>")
 }else if ((gender == "Boy" || gender =="boy") && (weather == "Hot" || weather == "hot") && (age == "Adult" || age =="adult") && (color == "Red" || color == "red")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img32.jpeg'>")
+    document.write("<img src='images/img32.jpeg' width='200' height='200'>")
 }else if ((gender == "Boy" || gender =="boy") && (weather == "Cold" || weather == "cold") && (age == "Adult" || age =="adult") && (color == "Blue" || color == "blue")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img33.jpeg'>")
+    document.write("<img src='images/img33.jpeg' width='200' height='200'>")
 }else if ((gender == "Boy" || gender =="boy") && (weather == "Hot" || weather == "hot") && (age == "Adult" || age =="adult") && (color == "Blue" || color == "blue")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img34.jpeg'>")
+    document.write("<img src='images/img34.jpeg' width='200' height='200'>")
 }else if ((gender == "Boy" || gender =="boy") && (weather == "Cold" || weather == "cold") && (age == "Adult" || age =="adult") && (color == "Green" || color == "green")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img35.jpeg'>")
+    document.write("<img src='images/img35.jpeg' width='200' height='200'>")
 }else if ((gender == "Boy" || gender =="boy") && (weather == "Hot" || weather == "hot") && (age == "Teenager" || age =="teenager") && (color == "Green" || color == "green")){
     console.log("This is the outfit recommendation for you")
     document.write("THIS IS THE OUTFIT RECOMMENDATION FOR YOU 🤩: ")
-    document.write("<img src='images/img36.jpeg'>")
+    document.write("<img src='images/img36.jpeg' width='200' height='200'>")
 }
